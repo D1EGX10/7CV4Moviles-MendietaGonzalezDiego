53a6a8ccacf3a98328138ca4bcd9e397a9a919c0
D1EGX10 <dmendieta2005@gmail.com>
2026-09-25 20:19:25 -0600

    Parte1-EntradaDeTexto

--- Tarea2/Vxml/app/src/main/java/dmendieta2005/gmail/dmendieta2005/xmltarea2/entradadetexto.kt
+++ Tarea2/Vxml/app/src/main/java/dmendieta2005/gmail/dmendieta2005/xmltarea2/entradadetexto.kt
@@ -1,59 +1,46 @@
 package dmendieta2005.gmail.dmendieta2005.xmltarea2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [entradadetexto.newInstance] factory method to
- * create an instance of this fragment.
- */
 class entradadetexto : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_entradadetexto, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_entradadetexto, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment entradadetexto.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            entradadetexto().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val opciones = arrayOf("México", "Colombia", "Argentina", "España", "Perú")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, opciones)
+        val autoComplete = view.findViewById<AutoCompleteTextView>(R.id.autoCompleteOpciones)
+        autoComplete.setAdapter(adapter)
+
+        val inputError = view.findViewById<TextInputEditText>(R.id.inputError)
+        val layoutError = view.findViewById<TextInputLayout>(R.id.layoutError)
+        val errorMensaje = getString(R.string.error_codigo)
+
+        inputError.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null && s.length < 5) {
+                    layoutError.error = errorMensaje
+                } else {
+                    layoutError.error = null
                 }
             }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        return view
     }
 }
