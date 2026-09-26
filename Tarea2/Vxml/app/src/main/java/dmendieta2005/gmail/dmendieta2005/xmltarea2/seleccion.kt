22e828ff9725e0eeb63f8fdf0ec7f86585699a0e
D1EGX10 <dmendieta2005@gmail.com>
2026-09-25 20:33:46 -0600

    Parte3-Seleccion

--- Tarea2/Vxml/app/src/main/java/dmendieta2005/gmail/dmendieta2005/xmltarea2/seleccion.kt
+++ Tarea2/Vxml/app/src/main/java/dmendieta2005/gmail/dmendieta2005/xmltarea2/seleccion.kt
@@ -1,59 +1,50 @@
 package dmendieta2005.gmail.dmendieta2005.xmltarea2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [seleccion.newInstance] factory method to
- * create an instance of this fragment.
- */
 class seleccion : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_seleccion, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_seleccion, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment seleccion.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            seleccion().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val spinner = view.findViewById<Spinner>(R.id.spinnerSeleccion)
+        val opcionesSpinner = arrayOf("Opción 1", "Opción 2", "Opción 3")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, opcionesSpinner)
+        spinner.adapter = adapter
+
+        val btnFecha = view.findViewById<MaterialButton>(R.id.btnFecha)
+        val btnHora = view.findViewById<MaterialButton>(R.id.btnHora)
+
+        btnFecha.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText(getString(R.string.btn_fecha))
+                .build()
+            datePicker.show(parentFragmentManager, "DATE_PICKER")
+        }
+
+        btnHora.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText(getString(R.string.btn_hora))
+                .build()
+            timePicker.show(parentFragmentManager, "TIME_PICKER")
+        }
+
+        return view
     }
 }
